5f817f6f570dd56c35e3399aca9ef4f7448b1eed
artemkaint <artemkaint@nodasoft.com>
2016-08-09 06:27:54 +0000

    Remove unused dependencies in final code

--- src/decorator.js
+++ src/decorator.js
@@ -9,7 +9,10 @@ export default function childByRefWrapper(ComposedComponent) {
 
     render() {
       const byRef = _.keyBy(React.Children.toArray(this.props.children), 'ref');
-      return <ComposedComponent {...this.props} childrenByRef={byRef}/>;
+      return React.createElement( ComposedComponent, {
+        ...this.props,
+        childrenByRef: byRef
+      });
     }
 
     displayName = `ChildByRefWrapper(${ComposedComponent.displayName || ComposedComponent.name || 'Component'})`
